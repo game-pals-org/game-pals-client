81f3c966d4d2be1e7d1215034b35371c535ce27c
Kamil Wisniewski <kamil.m.wis@gmail.com>
2021-07-01 14:02:42 +0200

    added registering functionality

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {LoginRegisterInfo} from "../../model/login-register-info.model";
+import {AccountService} from "../../service/account.service";
 
 @Component({
   selector: 'app-sign-in',
@@ -8,7 +11,29 @@ import { Component, OnInit } from '@angular/core';
 export class SignInComponent implements OnInit {
 
 
-  constructor() { }
+  public message: string = '';
+
+  public signingInForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  public onSelect () {
+    const username = this.signingInForm.value.username;
+    const password = this.signingInForm.value.password;
+    let registerInfo: LoginRegisterInfo = {success: false, username: '', message: ''};
+    this.accountService.registerUser(username, password).subscribe(
+      (loginRegisterInfo: LoginRegisterInfo) => {
+        registerInfo = loginRegisterInfo
+      }
+    )
+
+    this.signingInForm.reset();
+
+    this.message = registerInfo.message;
+  }
+
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
